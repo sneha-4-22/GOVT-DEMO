fe949be4609fb998541a768a7e525ee7d5dd91b7
sneha kumari <snehakumari64445@gmail.com>
2025-11-01 20:03:11 +0530

    Sync local folder with GitHub

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,11 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { UserProvider } from './contexts/UserContext';
-import LandingPage from './components/LandingPage';
-import LoginRegister from './components/LoginRegister';
-import Dashboard from './components/Dashboard';
-import ProtectedRoute from './components/ProtectedRoute';
-import EmailVerification from './components/EmailVerification';
-import PaymentSuccess from './components/PaymentSuccess';
+
+import MGNREGADashboard from './component/MGNREGADashboard';
+
 function App() {
   return (
-    <Router>
-      <UserProvider>
-        <Routes>
-          {/* Public routes */}
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/login" element={<LoginRegister />} />
-          <Route path="/verify-email" element={<EmailVerification />} />
-          
-          {/* Protected routes */}
-          <Route 
-            path="/dashboard" 
-            element={
-              <ProtectedRoute>
-                <Dashboard />
-              </ProtectedRoute>
-            } 
-          />
-           <Route path="/payment-success" element={<PaymentSuccess />} />
-          {/* Catch all other routes */}
-          <Route path="*" element={<LandingPage />} />
-        </Routes>
-      </UserProvider>
-    </Router>
+    <div className="App">
+      <MGNREGADashboard />
+    </div>
   );
 }
 
